2847e587b3eaedb6274ef30755ffda354673296e
gaoshuzhendanten <13720970438@163.com>
2022-05-06 20:46:59 +0800

    add menu 1.1

--- home/static/js/src/zbase.js
+++ home/static/js/src/zbase.js
@@ -1,4 +1,15 @@
 class AcGame{
     constructor(id){
+        console.log("start init");
+        this.id = id;
+        this.$ac_game = $('#' + id);
+        this.menu = new AcGameMenu(this);
+        this.playground = new AcGamePlayground(this);
+        console.log("init acgame");
+        this.start();
+    }
+
+    start(){
+        
     }
 }
